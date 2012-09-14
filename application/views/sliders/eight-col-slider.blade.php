eb67a6e90829b353d0d74eca170381af6ad91ded
John Kevin Basco <basco.johnkevin@gmail.com>
2012-09-13 19:53:24 -0700

    transfer slider view template to sliders folder

---
+++ application/views/sliders/eight-col-slider.blade.php
@@ -0,0 +1,150 @@
+<link rel="stylesheet" href="css/ecs.css" media="all"  type="text/css"/> 
+
+
+<div id="ecs-con">
+	<div id="ecs">
+		<ul>
+			<li id="slide-1" class="slides">
+				<div class="images first-image">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images first-image">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+			</li><!-- slide-1 -->
+
+			<li id="slide-2" class="slides">
+				<div class="images first-image">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images first-image">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				
+			</li><!-- slide-2 -->
+
+			<li id="slide-3" class="slides">
+				<div class="images first-image">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images first-image">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				<div class="images">
+					<a href="#">
+						<img src="img/eight-col-slider/sample.jpg" width="163" height="163">
+					</a>
+				</div>
+				
+			</li><!-- slide-3 -->
+
+			
+
+
+		</ul>
+		
+	</div><!-- ecs -->
+
+	<div id="ecs-nav">
+		<button id="prev-btn" data-dir="prev">previous</button>
+		<button id="next-btn" data-dir="next">next</button>
+	</div>
+
+</div><!-- ecs-con -->
