519593b3b87facac5be8835d0fb9092c6167029f
John Kevin Basco <basco.johnkevin@gmail.com>
2012-09-17 04:32:28 -0700

    configure dbase config

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ return array(
 		'mysql' => array(
 			'driver'   => 'mysql',
 			'host'     => 'localhost',
-			'database' => 'linq_db',
+			'database' => 'property_site',
 			'username' => 'root',
 			'password' => '',
 			'charset'  => 'utf8',
