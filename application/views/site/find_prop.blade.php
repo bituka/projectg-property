fefb90cae25a6247e44a22d0f9fb528aa733d288
goryo <goryo.webdev@gmail.com>
2012-09-18 21:21:39 +0400

    layout find property

--- application/views/site/find_prop.blade.php
+++ application/views/site/find_prop.blade.php
@@ -8,13 +8,33 @@
 		<div>
 			<label>Suburbs, States, or Postcodes</label><br /><br />
 			<input type="text" value="e.g. Melbourn City" name="f" id="f" onfocus="defaultInput(this)" 	="clearInput(this)">
-			<!--<input type="submit" id="searchsubmit" value=" " />-->
-			<input type="button" value="submit" />
+			<br /><br />
+			<span style="color: #bb8930; float: right;"><input type="submit" id="playbutton" value="" /> SEARCH</span>
 		</div>
 	</form>
-		
 </div>
- 
-  
+<br />
+<div id="filter-menu">
+<ul>
+	<span style="background-color: #f5a604;"><li>MOST RECENT</li></span>
+	<span style="background-color: white; color:#f5a604"><li>MOST POPULAR</li></span>
+	<li>Filter by:</li>
+	<li>All</li> 
+	<li>House</li> 
+	<li>Apartment & Unit</li> 
+	<li>Apartment Unit</li> 
+	<li>Townhouse</li> 
+	<li>Villa</li>
+	<li>Block of Units</li>
+</ul>
+</div> 
+
+
+
+
+<div id="ecs-nav">
+		<button id="prev-btn" class="with-hover" data-dir="prev">&lt; previous</button>
+		<button id="next-btn" class="with-hover" data-dir="next">next &gt;</button>
+</div>  
 	
 @endsection
