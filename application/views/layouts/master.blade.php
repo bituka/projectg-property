5f76f97fb53e0b10a5ce1e4f27145b7b107d9c92
John Kevin Basco <basco.johnkevin@gmail.com>
2012-10-14 23:54:34 -0700

    added a watermark in the search form in find a property page, make the search by state in the find a prop page work when in filter by ALL, cleaned up and make the assets relative to public folder to avoid request errors in other pages

--- application/views/layouts/master.blade.php
+++ application/views/layouts/master.blade.php
@@ -5,11 +5,11 @@
 		<title>{{ $title }}</title>
 		
 		<!-- CSS -->
-		<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
-		<link rel="stylesheet" href="css/social-icons.css" type="text/css" media="screen" />
-		<link rel="stylesheet" href="css/ecs.css" media="all"  type="text/css"/>
+		<link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css" media="screen" />
+		<link rel="stylesheet" href="{{ asset('css/social-icons.css') }}" type="text/css" media="screen" />
+		<link rel="stylesheet" href="{{ asset('css/ecs.css') }}" media="all"  type="text/css"/>
 		<!--[if IE 8]>
-			<link rel="stylesheet" type="text/css" media="screen" href="css/ie8-hacks.css" />
+			<link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/ie8-hacks.css') }}" />
 		<![endif]-->
 		<!-- ENDS CSS -->	
 
@@ -18,22 +18,18 @@
 		<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>-->
 		
 		<!-- JS -->
-		<script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
-		<script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
-		<script type="text/javascript" src="js/easing.js"></script>
-		<script type="text/javascript" src="js/jquery.scrollTo-1.4.2-min.js"></script>
-		<script type="text/javascript" src="js/jquery.cycle.all.js"></script>
-		<script type="text/javascript" src="js/custom.js"></script>
+		<script type="text/javascript" src="{{ asset('js/jquery-1.5.1.min.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('js/jquery-ui-1.8.13.custom.min.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('js/easing.js') }}"></script>
 		
-		<!-- Isotope -->
-		<script src="js/jquery.isotope.min.js"></script>
+		
 		
 		<!--[if IE]>
 			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 		
 		<!--[if IE 6]>
-			<script type="text/javascript" src="js/DD_belatedPNG.js"></script>
+			<script type="text/javascript" src="{{ asset('js/DD_belatedPNG.js') }}"></script>
 			<script>
 	      		/* EXAMPLE */
 	      		//DD_belatedPNG.fix('*');
@@ -42,56 +38,19 @@
 		<!-- ENDS JS -->
 		
 		
-		<!-- Nivo slider -->
-		<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
-		<script src="js/nivo-slider/jquery.nivo.slider.js" type="text/javascript"></script>
-		<!-- ENDS Nivo slider -->
-		
-		<!-- tabs -->
-		<link rel="stylesheet" href="css/tabs.css" type="text/css" media="screen" />
-		<script type="text/javascript" src="js/tabs.js"></script>
-  		<!-- ENDS tabs -->
-  		
-  		<!-- prettyPhoto -->
-		<script type="text/javascript" src="js/prettyPhoto/js/jquery.prettyPhoto.js"></script>
-		<link rel="stylesheet" href="js/prettyPhoto/css/prettyPhoto.css" type="text/css" media="screen" />
-		<!-- ENDS prettyPhoto -->
-		
-		<!-- superfish -->
-		<link rel="stylesheet" media="screen" href="css/superfish.css" /> 
-		<link rel="stylesheet" media="screen" href="css/superfish-left.css" /> 
-		<script type="text/javascript" src="js/superfish-1.4.8/js/hoverIntent.js"></script>
-		<script type="text/javascript" src="js/superfish-1.4.8/js/superfish.js"></script>
-		<script type="text/javascript" src="js/superfish-1.4.8/js/supersubs.js"></script>
-		<!-- ENDS superfish -->
-		
-		<!-- poshytip -->
-		<link rel="stylesheet" href="js/poshytip-1.0/src/tip-twitter/tip-twitter.css" type="text/css" />
-		<link rel="stylesheet" href="js/poshytip-1.0/src/tip-yellowsimple/tip-yellowsimple.css" type="text/css" />
-		<script type="text/javascript" src="js/poshytip-1.0/src/jquery.poshytip.min.js"></script>
-		<!-- ENDS poshytip -->
-		
-		<!-- Tweet -->
-		<link rel="stylesheet" href="css/jquery.tweet.css" media="all"  type="text/css"/> 
-		<script src="js/tweet/jquery.tweet.js" type="text/javascript"></script> 
-		<!-- ENDS Tweet -->
-		
-		<!-- Fancybox -->
-		<link rel="stylesheet" href="js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
-		<script type="text/javascript" src="js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-		<!-- ENDS Fancybox -->
-		
 		<!-- eight column slider -->
-		<script type="text/javascript" src="js/eight-col-slider.js"></script>
+		<script type="text/javascript" src="{{ asset('js/eight-col-slider.js') }}"></script>
 		<!-- ENDS eight column slider -->
 
 		<!-- css transitions -->
-		<script type="text/javascript" src="js/css-trans.js"></script>
+		<script type="text/javascript" src="{{ asset('js/css-trans.js') }}"></script>
 		<!-- ENDS css transitions -->
 
 		<!-- site master script -->
-		<script type="text/javascript" src="js/site.js"></script>
+		<script type="text/javascript" src="{{ asset('js/site.js') }}"></script>
 		<!-- ENDS site master script -->
+
+		<script type="text/javascript" src="{{ asset('js/jquery.watermark.min.js') }}"></script>
 
 	</head>
 <body id="{{ $current_page }}">
--- application/views/site/second_find_prop.blade.php
+++ application/views/site/second_find_prop.blade.php
@@ -1,4 +1,4 @@
-@layout('layouts.second_master')
+@layout('layouts.master')
 
 @section('content')
 <div class="find_prop">
@@ -7,7 +7,15 @@
 	<form method="get" id="searchform" action="#">
 		<div  style="position: relative;">
 			<label>Search by States</label><br /><br />
-			<input type="text" value="e.g. Melbourn City" name="keyword" id="f" onfocus="defaultInput(this)" 	="clearInput(this)">
+			<input type="text" value="" name="keyword" id="f" onfocus="defaultInput(this)" 	="clearInput(this)">
+			
+			<!-- watermark script -->
+			<script type="text/javascript">
+				$('#f').watermark('e.g. Melbourn City');
+			</script>
+			<!-- watermark script ends -->
+
+
 			<br /><br />
 			<span style="color: #bb8930; float: right;"><input type="submit" id="playbutton" value="" /> SEARCH</span>
 		</div>
