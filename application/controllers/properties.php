15e11c5c1ebfa8ffec1b7a492a9fface270eb3e9
John Kevin Basco <basco.johnkevin@gmail.com>
2012-10-27 04:48:53 -0700

    cleaned up properties controller

--- application/controllers/properties.php
+++ application/controllers/properties.php
@@ -4,23 +4,8 @@ class Properties_Controller extends Base_Controller{
   
 	public $restful = true; 
 
-
-	/**
-	* render homepage
-	*/
-	// public function get_index()
-	// {
-	// 	$view = View::make('properties.index');
-
-	// 	$this->layout->title = "Linq Property: Home";
-
-	// 	$this->layout->main = $view;
-	// }	  
-
 	public function get_search($category)
 	{
-
-		// return Input::get('keyword');
 		$keyword = Input::get('keyword');
 
 		$view = View::make('site.second_find_prop');
@@ -28,9 +13,6 @@ class Properties_Controller extends Base_Controller{
 		$view['current_page']  = 'find-a-property';		
 
 		$cat = Category::where_name($category)->first();
-		// return print_r($cat);
-
-		
 
 		if (!empty($keyword)) {
 			$properties = Property::where_category_id_and_state($cat->id, $keyword)->paginate(12);
@@ -38,8 +20,8 @@ class Properties_Controller extends Base_Controller{
 			$properties = Property::where_category_id($cat->id)->paginate(12);
 		}
 		
-
-		$view['properties']  = $properties;		
+		$view['properties']  = $properties;
+				
 		return $view;
 	}
 
